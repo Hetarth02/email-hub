96a5e45fb61991577c2af80da5c87663b2423837
Hetarth02 <hetarth02@gmail.com>
2022-07-06 22:00:54 +0530

    Updated mail creds

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 use Throwable;
 use App\Mail\EmailTemplate;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+// use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
+    /**
+     * Send Email
+     *
+     * @param  mixed $request
+     * @return void
+     */
     public function sendMail(Request $request)
     {
         $input = $request->all();
@@ -18,15 +24,19 @@ class MailController extends Controller
                 ->cc($input['email_cc'] ?? null)
                 ->bcc($input['email_bcc'] ?? null)
                 ->send(new EmailTemplate($input));
-            return response()->json([
-                'status' => true,
-                'message' => 'Email sent Successfully!'
-            ]);
+            return response()->json(
+                [
+                    'status' => true,
+                    'message' => 'Email sent Successfully!'
+                ]
+            );
         } catch (Throwable $error) {
-            return response()->json([
-                'status' => false,
-                'message' => 'Something went wrong, please try again!'
-            ]);
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => 'Something went wrong, please try again!'
+                ]
+            );
         }
     }
 }
